79ca7092f30a1ccd6d8a6f8be688e94002b9b5f4
8084 <klntsky@gmail.com>
2019-03-31 21:36:29 +0300

    test for balancedFold

--- test/TestMain.purs
+++ test/TestMain.purs
@@ -13,7 +13,7 @@ import Data.Monoid.Additive
 import Data.Monoid.Multiplicative
 
 -- | A binary tree
-data T a = L a | B (T a) (T a)
+data T a = Empty | L a | B (T a) (T a)
 infixl 6 B as :*:
 
 derive instance eqT :: Eq a => Eq (T a)
@@ -21,7 +21,11 @@ derive instance eqT :: Eq a => Eq (T a)
 instance semigroupT :: Semigroup (T a) where
   append = B
 
+instance monoidT :: Monoid (T a) where
+  mempty = Empty
+
 instance showT :: Show a => Show (T a) where
+  show Empty = "Empty"
   show (L a) = "(L " <> show a <> ")"
   show (B a b) = "(" <> show a <> " :*: " <> show b <> ")"
 
@@ -50,6 +54,27 @@ main = runTest do
             equal (Just tree) (balancedFold' $ map L $ arr)
       assert "balanced fold of empty list is Nothing" $
         isNothing $ balancedFold' $ map L $ [] :: Array Unit
+      check [1]    $ L 1
+      check (1..2) $ (L 1) :*: (L 2)
+      check (1..3) $ (L 1) :*: ((L 2) :*: (L 3))
+      check (1..4) $ ((L 1) :*: (L 2)) :*: ((L 3) :*: (L 4))
+      check (1..5) $ ((L 1) :*: (((L 2) :*: (L 3)) :*: ((L 4) :*: (L 5))))
+      check (1..40) $ B (B (B (B (L 1) (L 2)) (B (L 3) (L 4)))
+                           (B (B (L 5) (L 6)) (B (L 7) (L 8))))
+                        (B (B (B (B (B (L 9) (L 10)) (B (L 11) (L 12)))
+                                 (B (B (L 13) (L 14)) (B (L 15) (L 16))))
+                              (B (B (B (L 17) (L 18)) (B (L 19) (L 20)))
+                                 (B (B (L 21) (L 22)) (B (L 23) (L 24)))))
+                           (B (B (B (B (L 25) (L 26)) (B (L 27) (L 28)))
+                                 (B (B (L 29) (L 30)) (B (L 31) (L 32))))
+                              (B (B (B (L 33) (L 34)) (B (L 35) (L 36)))
+                                 (B (B (L 37) (L 38)) (B (L 39) (L 40))))))
+
+  suite "balancedFold" do
+    test "satisfies assertions" do
+      let check arr tree =
+            equal tree (balancedFold $ map L $ arr)
+      check []     $ mempty
       check [1]    $ L 1
       check (1..2) $ (L 1) :*: (L 2)
       check (1..3) $ (L 1) :*: ((L 2) :*: (L 3))
